a52843e3c7274c8e5dc8d24672502800bcbaefae
Cassiano Bitencourt <tricecautopo@gmail.com>
2020-12-07 10:00:28 -0300

    Adding cors configs

--- application/__init__.py
+++ application/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
 
 from application.views.tables_view import bp as tables_view
 from application.views.teams_view import bp as teams_view
@@ -6,6 +7,9 @@ from application.views.teams_view import bp as teams_view
 
 def create_app():
     app = Flask(__name__)
+
+    cors = CORS(app)
+    app.config['CORS_HEADERS'] = 'Content-Type'
 
     app.register_blueprint(tables_view)
     app.register_blueprint(teams_view)
